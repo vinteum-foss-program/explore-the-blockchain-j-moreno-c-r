4362f0fa46ff0ae9e97df6d54f0ca6780abc9388
joaozinho <joaozinho@DESKTOP-PJQRVOQ.Dlink>
2024-11-22 11:50:13 -0300

    chalege 3

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
 # How many new outputs were created by block 123,456?
+bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_095 -rpcpassword=X19iRP1wRyAx  getblockstats 123456 | jq --raw-output .utxo_increase
