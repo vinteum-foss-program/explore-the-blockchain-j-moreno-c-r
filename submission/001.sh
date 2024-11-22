e3934320c50a1848b4f85a662a28517d4528caa0
j-moreno-c-r <joaomcr@proton.me>
2024-11-22 12:04:00 -0300

    chalege 001

--- submission/001.sh
+++ submission/001.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # What is the hash of block 654,321?
-bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_095 -rpcpassword=X19iRP1wRyAx getblockhash 654321
+bitcoin-cli getblockhash 654321
 
 
